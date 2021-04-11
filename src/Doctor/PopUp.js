44bbd1c7e01217b349254e9e7eb01c8c23408da9
dicompathakofficial <dicompathakofficial2001@gmail.com>
2021-04-11 14:01:04 +0530

    App

--- src/Doctor/PopUp.js
+++ src/Doctor/PopUp.js
@@ -17,7 +17,11 @@ export default function PopUp({setpopUp}) {
                     <ImCancelCircle size="2rem" color="#fff" />
                 </div>
                 <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. 
+                    <br />
+                    <br />
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
                 </p>
                 <div className={styles.inputButton}>
                     <BsArrowRight size="2.75rem" /> Continue Booking
