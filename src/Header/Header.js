56500c90d4a41a4e6158d4360c9dc5a0b0fa8599
dicompathakofficial <dicompathakofficial2001@gmail.com>
2021-04-11 13:14:56 +0530

    App

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,28 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import NavBar from './NavBar';
 import styles from '../../styles/Header/Header.module.css';
 import Doctors from '../Doctors/Doctors';
 
+const Headertext = ({showDoctors}) => {
+    return (
+        <div className={styles.HeaderContent}>
+            <h2>The Best </h2>
+            <h2 style={{ color: "#EA4949" }}>Vets</h2>
+            <h2>In Town .</h2>
+
+            <p>for the ones who truly care for you .</p>
+            <div className={styles.HeaderContentBtns}>
+                    <button onClick={showDoctors}>
+                    Explore Doctors
+                    </button>
+                <div>
+                    How We Verify Our Vets?
+                </div>
+            </div>
+        </div>
+    )
+}
+
 export default function Header() {
+    const [doctors, setdoctors] = useState(false);
+
+    const showDoctors = ()=> {
+        doctors ? setdoctors(false) : setdoctors(true);
+    }
+
     return (
         <div className={styles.Header}>
             <NavBar />
-            {/* <div className={styles.HeaderContent}>
-                <h2>The Best </h2>
-                <h2 style={{ color: "#EA4949" }}>Vets</h2>
-                <h2>In Town .</h2>
 
-                <p>for the ones who truly care for you .</p>
-                <div className={styles.HeaderContentBtns}>
-                    <button>
-                        Explore Doctors
-                    </button>
-                    <div>
-                        How We Verify Our Vets?
-                    </div>
-                </div>
-            </div> */}
-            <Doctors />
+            {
+                doctors ? <Doctors /> : <Headertext showDoctors={showDoctors}/>
+            }
 
         </div>
     )
