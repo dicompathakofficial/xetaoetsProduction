f227737bc86eb92ef5fbf6bf7c591de0fe526dfa
dicompathakofficial <dicompathakofficial2001@gmail.com>
2021-04-20 00:17:39 +0530

    App

---
+++ src/DoctorProfiles/DoctorHome/DoctorHome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../../../styles/DoctorProfiles/DoctorHome/DoctorHome.module.css';
+
+export default function DoctorHome() {
+    return (
+        <div className={styles.DoctorHome}>
+            
+        </div>
+    )
+}
