754968f29ad61a3af04462b4d047edf9331c0644
dicompathakofficial <dicompathakofficial2001@gmail.com>
2021-04-22 10:27:51 +0530

    App

--- src/DoctorProfiles/DoctorNav/DoctorNav.js
+++ src/DoctorProfiles/DoctorNav/DoctorNav.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../../../styles/DoctorProfiles/DoctorNav/DoctorNav.module.css';
 
 export default function DoctorNav() {
+
+    const [menu, setmenu] = useState(false);
+
+    const handleMenu = () => {
+        menu === true ? setmenu(false) : setmenu(true);
+    }
     return (
         <div className={styles.DoctorNav}>
             <div className={styles.DoctorNavContainer}>
                 <div className={styles.logo}>
                     <img src="./images/logo.png" />
                 </div>
-                <div className={styles.DoctorNavLogout}>
+                <div onClick={handleMenu} className={styles.DoctorNavLogout}>
                     Menu
+                    {
+                        menu && (
+                            <div className={styles.DoctorMenuContent}>
+                                <div>Appointments</div>
+                                <div>Create Listings</div>
+                                <div>Logout</div>
+                            </div>
+                        )
+                    }
+
                 </div>
             </div>
         </div>
     )
+}
+
+const MenuContent = () => {
+
 }
